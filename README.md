7ab11f742d83478194bae949a9ce2d4fb5b87895
den_pras <pras.rubicteam@gmail.com>
2024-03-17 20:23:51 +0700

    initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# mulang-ci3
---
+++ application/helpers/mulang_helper.php
@@ -0,0 +1,38 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+if (!function_exists('mulang')) {
+    function mulang($language = null)
+    {
+        $CI =& get_instance();
+
+        if ($language == null) {
+            $language = $CI->session->userdata('language') ? $CI->session->userdata('language') : 'id';
+        }
+
+        $CI->session->set_userdata('language', $language);
+
+        return $language;
+    }
+}
+
+if (!function_exists('load_mulang')) {
+    function load_mulang($language)
+    {
+        $CI =& get_instance();
+        $path = APPPATH . 'language/portal.json';
+        if (file_exists($path)) {
+            $json_content = file_get_contents($path);
+            $language_data = json_decode($json_content, true);
+            $language_messages = array();
+            foreach ($language_data as $key => $value) {
+                if (isset($value[$language])) {
+                    $language_messages[$key] = $value[$language];
+                }
+            }
+            return $language_messages;
+        } else {
+            return array(); // Return empty array if file doesn't exist
+        }
+    }
+}
